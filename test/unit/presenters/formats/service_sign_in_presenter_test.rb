0a659031411b5d0411319f708c0b7730beb26dc9
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2017-11-30 11:11:20 +0000

    Add rendering_app field to service_sign_in_presenter

--- test/unit/presenters/formats/service_sign_in_presenter_test.rb
+++ test/unit/presenters/formats/service_sign_in_presenter_test.rb
@@ -18,4 +18,8 @@ class ServiceSignInTest < ActiveSupport::TestCase
   should "[:schema_name]" do
     assert_equal 'service_sign_in', result[:schema_name]
   end
+
+  should "[:rendering_app]" do
+    assert_equal 'government-frontend', result[:rendering_app]
+  end
 end
